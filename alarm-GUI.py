836a15a167745517bd9881cf89eca3f1b04ff0ba
GangMS97 <bmw9813@gmail.com>
2021-04-28 17:07:22 +0900

    Make list button and list view box

--- alarm-GUI.py
+++ alarm-GUI.py
@@ -5,61 +5,73 @@ import time
 import os
 from tkinter import messagebox
 
-#def main():
+# def main():
 root = Tk()
 root.title("Alarm clock")
+
+
 def SubmitButton():
-  AlarmTime= entry1.get()
-  Message1()
-  #label2.config(text ="The Alarm will Ring at {} ".format(AlarmTime))  #delayed in execution
-  CurrentTime = time.strftime("%H:%M")
-  print("the alarm time is: {}".format(AlarmTime))
-  #label2.config(text="")
-  while AlarmTime != CurrentTime:
-    #label2.config(text ="The Alarm will Ring at {} ".format(AlarmTime))
+    AlarmTime = entry1.get()
+    Message1()
+    # label2.config(text ="The Alarm will Ring at {} ".format(AlarmTime))  #delayed in execution
     CurrentTime = time.strftime("%H:%M")
-    time.sleep(1)
-  if AlarmTime == CurrentTime:
-     print("now Alarm Musing Playing")
-     os.system("start alarm-music.mp3")
-     label2.config(text = "Alarm music playing.....")
-     messagebox.showinfo(title= 'Alarm Message', message= "{}".format(entry2.get()))
+    print("the alarm time is: {}".format(AlarmTime))
+    # label2.config(text="")
+    while AlarmTime != CurrentTime:
+        # label2.config(text ="The Alarm will Ring at {} ".format(AlarmTime))
+        CurrentTime = time.strftime("%H:%M")
+        time.sleep(1)
+
+    if AlarmTime == CurrentTime:
+        print("now Alarm Musing Playing")
+        os.system("start alarm-music.mp3")
+        label2.config(text="Alarm music playing.....")
+        label3.config(text=format(AlarmTime))
+        messagebox.showinfo(title='Alarm Message', message="{}".format(entry2.get()))
+
+def ListButton():
+    AlarmTime = entry1.get()
+    Message1()
+    messagebox.showinfo(title='Alarm List', message="{}".format(entry1.get()))
+
+
+
 def Message1():
-    AlarmTimeLable= entry1.get()
+    AlarmTimeLable = entry1.get()
     label2.config(text="the Alarm time is Counting...")
-    #label2.config(text= "the Alarm will ring at {}".format(AlarmTimeLable))
-    messagebox.showinfo(title = 'Alarm clock', message = 'Alarm will Ring at {}'.format(AlarmTimeLable))     
+    # label2.config(text= "the Alarm will ring at {}".format(AlarmTimeLable))
+    messagebox.showinfo(title='Alarm clock', message='Alarm will Ring at {}'.format(AlarmTimeLable))
+
+
 frame1 = ttk.Frame(root)
 frame1.pack()
-frame1.config(height = 100, width = 100)
+frame1.config(height=100, width=100)
 
-label1= ttk.Label(frame1,text = "Enter the Alarm time :")
+label1 = ttk.Label(frame1, text="Enter the Alarm time :")
 label1.pack()
 
-
-entry1 = ttk.Entry(frame1, width = 30)
+entry1 = ttk.Entry(frame1, width=30)
 entry1.pack()
-entry1.insert(3,"example - 13:15")
+entry1.insert(3, "example - 12:29")
 
-labelAlarmMessage= ttk.Label(frame1, text="Alarm Message:")
+labelAlarmMessage = ttk.Label(frame1, text="Alarm Message:")
 labelAlarmMessage.pack()
 
-entry2= ttk.Entry(frame1, width=30)
+entry2 = ttk.Entry(frame1, width=30)
 entry2.pack()
 
-button1= ttk.Button(frame1, text= "submit", command=SubmitButton)
+button1 = ttk.Button(frame1, text="submit", command=SubmitButton)
 button1.pack()
 
-labelAlarmList= ttk.Label(frame1, text= format(AlarmTimeLable))
-labelAlarmList.pack()
-
-#this Label2 will show the Last Alarm Time
-label2= ttk.Label(frame1)
+button2 = ttk.Button(frame1, text="list", command=ListButton)
+button2.pack()
+# this Label2 will show the Last Alarm Time
+label2 = ttk.Label(frame1)
 label2.pack()
+label3 = ttk.Label(frame1)
+label3.pack()
 
-    
-#label2.config(text="hello")
+# label2.config(text="hello")
 
 root.mainloop()
 
-# 343431414aaaa
